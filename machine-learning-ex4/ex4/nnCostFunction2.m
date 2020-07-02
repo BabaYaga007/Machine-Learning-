7af5c28bd15548b382a22a271d8588657dc98778
BabaYaga007 <achint.179301016@muj.manipal.edu>
2020-07-02 21:01:33 +0530

    Week 5

--- machine-learning-ex4/ex4/nnCostFunction2.m
+++ machine-learning-ex4/ex4/nnCostFunction2.m
@@ -14,8 +14,8 @@ function [J grad] = nnCostFunction(nn_params, ...
 %   partial derivatives of the neural network.
 %
 
-% Reshape nn_params back into the parameters Theta1 and Theta2, the weight matrices
-% for our 2 layer neural network
+% Reshape nn_params back into the parameters Theta1 and Theta2, the weight 
+% matrices for our 2 layer neural network
 Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
                  hidden_layer_size, (input_layer_size + 1));
 
@@ -38,7 +38,30 @@ Theta2_grad = zeros(size(Theta2));
 %         variable J. After implementing Part 1, you can verify that your
 %         cost function computation is correct by verifying the cost
 %         computed in ex4.m
-%
+
+% FEED FORWARDING 
+a1 = [ones(m,1) X];
+z2 = a1*Theta1';
+a2 = sigmoid(z2);
+
+a2 = [ones(size(a2,1),1) a2];
+z3 = a2*Theta2';
+a3 = sigmoid(z3);
+hypo = a3;
+
+% y(k) - the great trick - we need to recode the labels as vectors 
+% containing only values 0 or 1 (page 5 of ex4.pdf)
+
+yVec = zeros(m,num_labels); % 5000*10
+for i=1:m
+  yVec(i,y(i))=1;
+end
+
+% COST FUNCTION
+cost = (1/m)* sum(sum((-yVec .* log(hypo)) - (1-yVec) .* log(1-hypo)));
+J = cost;
+
+
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
 %         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
@@ -54,81 +77,51 @@ Theta2_grad = zeros(size(Theta2));
 %               over the training examples if you are implementing it for the 
 %               first time.
 %
+
+for i = 1:m
+    % Step 1
+    a1 = [ones(m,1) X]; % 5000*401
+    z2 = a1*Theta1'; %5000*25
+    a2 = sigmoid(z2);
+    a2 = [ones(size(a2,1),1) a2]; % 5000*26
+    z3 = a2*Theta2'; % 5000*10
+    a3 = sigmoid(z3);
+    
+    % yVec = 5000*10
+    
+    % Step 2
+    del3 = a3 - yVec; % 5000*10
+    
+    % Step 3 
+    del2 = (del3*Theta2) .* [ones(size(z2,1),1) sigmoidGradient(z2)]; %5000*26
+    del2 = del2(:,2:end); % 5000*25
+    
+    % Step 4
+    Theta1_grad = Theta1_grad + del2'*a1; % 25 x 401
+    Theta2_grad = Theta2_grad + del3'*a2; % 10 x 26
+        
+end
+
+Theta1_grad =(1/m)* Theta1_grad;
+Theta2_grad = (1/m)* Theta2_grad;
+
+
 % Part 3: Implement regularization with the cost function and gradients.
 %
 %         Hint: You can implement this around the code for
 %               backpropagation. That is, you can compute the gradients for
 %               the regularization separately and then add them to Theta1_grad
 %               and Theta2_grad from Part 2.
-%
 
+%Regularized cost
+regTheta1 = Theta1(:,2:end);
+regTheta2 = Theta2(:,2:end);
 
-%% Part 1 implementation
+regcost = (lambda/(2*m))*(sum(sum(regTheta1.^2)) + sum(sum(regTheta2.^2)));
+J = J+regcost;
 
-a1 = [ones(m, 1) X];
-
-z2 = a1 * Theta1';
-a2 = sigmoid(z2);
-a2 = [ones(size(a2,1), 1) a2];
-
-z3 = a2 * Theta2';
-a3 = sigmoid(z3);
-hThetaX = a3;
-
-yVec = zeros(m,num_labels);
-
-for i = 1:m
-    yVec(i,y(i)) = 1;
-end
-
-% for i = 1:m
-%     
-%     term1 = -yVec(i,:) .* log(hThetaX(i,:));
-%     term2 = (ones(1,num_labels) - yVec(i,:)) .* log(ones(1,num_labels) - hThetaX(i,:));
-%     J = J + sum(term1 - term2);
-%     
-% end
-% 
-% J = J / m;
-
-J = 1/m * sum(sum(-1 * yVec .* log(hThetaX)-(1-yVec) .* log(1-hThetaX)));
-
-regularator = (sum(sum(Theta1(:,2:end).^2)) + sum(sum(Theta2(:,2:end).^2))) * (lambda/(2*m));
-
-J = J + regularator;
-
-%% Part 2 implementation
-
-
-
-for t = 1:m
-
-	% For the input layer, where l=1:
-	a1 = [1; X(t,:)'];
-
-	% For the hidden layers, where l=2:
-	z2 = Theta1 * a1;
-	a2 = [1; sigmoid(z2)];
-
-	z3 = Theta2 * a2;
-	a3 = sigmoid(z3);
-
-	yy = ([1:num_labels]==y(t))';
-	% For the delta values:
-	delta_3 = a3 - yy;
-
-	delta_2 = (Theta2' * delta_3) .* [1; sigmoidGradient(z2)];
-	delta_2 = delta_2(2:end); % Taking of the bias row
-
-	% delta_1 is not calculated because we do not associate error with the input    
-
-	% Big delta update
-	Theta1_grad = Theta1_grad + delta_2 * a1';
-	Theta2_grad = Theta2_grad + delta_3 * a2';
-end
-
-Theta1_grad = (1/m) * Theta1_grad + (lambda/m) * [zeros(size(Theta1, 1), 1) Theta1(:,2:end)];
-Theta2_grad = (1/m) * Theta2_grad + (lambda/m) * [zeros(size(Theta2, 1), 1) Theta2(:,2:end)];
+Theta1_grad= Theta1_grad + (lambda/m)*[zeros(size(Theta1,1),1) Theta1(:,2:end)];
+Theta2_grad= Theta2_grad + (lambda/m)*[zeros(size(Theta2,1),1) Theta2(:,2:end)];
 
 
 
